c5abb62639d96afbdcb78059a3ed46c96785e82c
Hasan Siddique <hasan.siddique99@gmail.com>
2017-02-16 20:54:18 +0000

    final

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ TinyTurtle.apply(window);
 /*global stamp*/
 /*global forward*/
 /*global right*/
+/*global left*/
 forward(50);
 right(90);
 forward(90);
@@ -19,4 +20,22 @@ forward(50);
 right(65);
 forward(50);
 right(90);
-stamp();
+forward(50);
+right(90);
+forward(25);
+right(90);
+forward(15);
+right(90);
+forward(25);
+left(90);
+forward(35);
+left(90);
+forward(50);
+left(65);
+forward(20);
+right(65);
+forward(10);
+right(90);
+forward(10);
+right(90);
+forward(14);
