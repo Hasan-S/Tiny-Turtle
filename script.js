f9fec8077219515ccc15b2ed70b4781ca5a4c0cf
Hasan Siddique <hasan.siddique99@gmail.com>
2017-02-16 20:36:13 +0000

    finished

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+/*global TinyTurtle*/
 TinyTurtle.apply(window);
+/*global stamp*/
+/*global forward*/
+/*global right*/
+forward(50);
+right(90);
+forward(90);
+right(90);
+forward(50);
+right(90);
+forward(90);
+right(90);
+forward(50);
+right(65);
+forward(50);
+right(50);
+forward(50);
+right(65);
+forward(50);
+right(90);
+stamp();
